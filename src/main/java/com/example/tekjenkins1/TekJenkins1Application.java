b2dcc9be5ba9d93f107acfe96acaf757b8a71fdd
petr1940 <petr1940@stud.kea.dk>
2021-10-19 15:01:57 +0200

    message

---
+++ src/main/java/com/example/tekjenkins1/TekJenkins1Application.java
@@ -0,0 +1,13 @@
+package com.example.tekjenkins1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekJenkins1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TekJenkins1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tekjenkins1/TekJenkins1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tekjenkins1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekJenkins1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
